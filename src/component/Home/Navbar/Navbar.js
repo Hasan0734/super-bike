ba53d2e38d1fcb8b3faf4e70bcc7c5e9ad32dd49
Jahid Hasan <jahidhasan2856@gmail.com>
2021-10-31 07:43:22 +0600

    navbar added

--- src/component/Home/Navbar/Navbar.js
+++ src/component/Home/Navbar/Navbar.js
@@ -3,58 +3,55 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useState } from 'react';
 
 const Navbar = () => {
-    const [isOpen, setisOpen] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <div className="flex">
-            <div className="fixed z-40 w-10/12">
-                <div className={`bg-gray-200 h-screen   ${isOpen ? 'block' : 'hidden'}`}>
+        <nav className="bg-green-500 py-3 px-4">
+            <div className="max-w-6xl mx-auto ">
 
-                    <div className="flex ">
-                        <button className="bg-gray-500  hover:bg-green-500 py-5">Menu</button>
-                        <button className="bg-gray-500  hover:bg-green-500 py-5">Account</button>
-                        <button className="bg-gray-500  hover:bg-green-500 py-5">Settings</button>
+
+                <div className={`flex transition duration-200 justify-${isOpen ? 'end' : 'between'}`}>
+                    <div className="flex space-x-4">
+
+
+
+                        <div className='flex space-x-3 items-center'>
+                            {/* menu button */}
+                            <button onClick={() => setIsOpen(!isOpen)} className="text-2xl font-bold text-white py-1"><FontAwesomeIcon icon={faBars} /></button>
+
+                            {/* logo */}
+                            <a className={`${isOpen ? 'hidden' : 'block'} text-2xl font-bold text-white`} href="#"><FontAwesomeIcon className="text-black" icon={faBiking} /> Super Bike</a>
+
+                        </div>
+
+                        {/* primary nav */}
+
+                        <div className="hidden md:flex flex items-center space-x-1 text-white">
+                            <a className="py-2 px-2" href="#">Home</a>
+                            <a className="py-2 px-2" href="#">Touring Bikes</a>
+                            <a className="py-2 px-2" href="#">Bicycle</a>
+                            <a className="py-2 px-2" href="#">Road Bikes</a>
+                            <a className="py-2 px-2" href="#">Cycle Pumps</a>
+                            <a className="py-2 px-2" href="#">Blog</a>
+                        </div>
+
+                        {/* secondary nav */}
+
                     </div>
-                    <ul>
-                        <li>Home</li>
-                        <li>Home</li>
-                        <li>Home</li>
-                        <li>Home</li>
-                    </ul>
+                    <div className={`${isOpen ? 'hidden' : 'block'} flex items-center space-x-3 `}>
+                        <ul className="flex space-x-5 text-xl items-center justify-center">
+                            <li><FontAwesomeIcon className="cursor-pointer " icon={faSearch} /></li>
+                            <li><FontAwesomeIcon className="cursor-pointer" icon={faUserAlt} /></li>
+                            <li><a className="relative" href="#"><FontAwesomeIcon icon={faShoppingBasket} /><span className="bg-yellow-300 text-sm absolute mt-2 -ml-3 rounded-full inline-flex justify-center items-center w-3 h-3">0</span></a></li>
+                        </ul>
+                    </div>
+
 
                 </div>
+
             </div>
-            <nav className="bg-green-500 p-5 flex-1">
-                <div className="container">
-                    <div className={`flex items-center justify-${isOpen ? 'end' : 'between'}`}>
-                        <div className="flex space-x-5 text-2xl text-white font-semibold items-center">
-                            <span onClick={() => setisOpen(!isOpen)} className="cursor-pointer lg:hidden md:hidden sm:block"><FontAwesomeIcon icon={faBars} /></span>
-                            <h1 className={`${isOpen ? 'hidden' : 'block'}`}><span className="text-gray-600"><FontAwesomeIcon icon={faBiking} /></span> SuperBike</h1>
-                        </div>
+        </nav>
 
-                        <div className={`flex space-x-10 ${isOpen ? 'hidden' : 'block'}`}>
-                            <div className="sm:hidden md:block">
-                                <div className="flex space-x-3 md:space-x-3 lg:space-x-8  text-white text-base ">
-                                    <a className="hover:text-gray-600" href="#">Home</a>
-                                    <a className="hover:text-gray-600" href="#">Touring Bikes</a>
-                                    <a className="hover:text-gray-600" href="#">Bicycle</a>
-                                    <a className="hover:text-gray-600" href="#">Road Bikes</a>
-                                    <a className="hover:text-gray-600" href="#">Blog</a>
-                                </div>
-                            </div>
-                            <ul className="flex space-x-5 text-xl items-center justify-center">
-                                <li><FontAwesomeIcon className="cursor-pointer " icon={faSearch} /></li>
-                                <li><FontAwesomeIcon className="cursor-pointer" icon={faUserAlt} /></li>
-                                <li><a className="relative" href="#"><FontAwesomeIcon icon={faShoppingBasket} /><span className="bg-yellow-300 text-sm absolute mt-2 -ml-3 rounded-full inline-flex justify-center items-center w-3 h-3">0</span></a></li>
-                            </ul>
-                        </div>
-                    </div>
-
-                </div>
-
-
-            </nav>
-        </div>
     );
 };
 
